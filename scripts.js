fc2df0048d18df780798eda5387e93b693354d80
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-11-29 03:56:03 -0500

    matchmedia

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-document.body.setAttribute('moo', ''); //set phony boolean attribute
-
 const appSidebar = document.querySelector('.app-sidebar');
 const appScrim = document.querySelector('.app-scrim');
 const appTopbarNavicon = document.querySelector('.app-topbar-navicon');
@@ -16,12 +14,38 @@ appTopbarNavicon.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     console.log('foo');
-    if(appSidebar.hasAttribute('persistent')) {
+    if(appSidebar.hasAttribute('persistent') && appSidebar.hasAttribute('opened')) {
+        appSidebar.removeAttribute('opened','');
         appSidebarContainer.removeAttribute('opened','');
         appMiniGuide.removeAttribute('hidden','');
         appMain.removeAttribute('guide-persistent-and-visible','');
         appMain.setAttribute('mini-guide-visible','');
-    } else {
-
+    } else if (appSidebar.hasAttribute('persistent')) {
+        appSidebar.setAttribute('opened','');
+        appSidebarContainer.setAttribute('opened','');
+        appMiniGuide.setAttribute('hidden','');
+        appMain.setAttribute('guide-persistent-and-visible','');
+        appMain.removeAttribute('mini-guide-visible','');
     }
+
 }
+
+
+//screen resizing
+
+var mql = window.matchMedia('(max-width: 600px)');
+
+function screenTest(e) {
+  if (e.matches) {
+    /* the viewport is 600 pixels wide or less */
+    appScrim.setAttribute('visible','');
+    console.log('This is a narrow screen — less than 600px wide.');
+  } else {
+    /* the viewport is more than than 600 pixels wide */
+    appScrim.removeAttribute('visible','');
+    console.log('This is a wide screen — more than 600px wide.');
+  }
+}
+
+screenTest(mql);
+mql.addListener(screenTest);

5b90f0df2745ea2b95b63374ddb881f07f598dfd
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-11-29 01:18:53 -0500

    cleanup

--- scripts.js
+++ scripts.js
@@ -3,16 +3,14 @@ document.body.setAttribute('moo', ''); //set phony bbolean attribute
 const appSidebar = document.querySelector('.app-sidebar');
 const appScrim = document.querySelector('.app-scrim');
 const appTopbarNavicon = document.querySelector('.app-topbar-navicon');
-
 const appMiniGuide = document.querySelector('.app-mini-guide');
-
 const appSidebarContainer = document.querySelector('.app-sidebar-container');
-
 const appGuideNavicon = document.querySelector('.app-guide-navicon');
 const appGuideSpacer = document.querySelector('.app-guide-spacer');
 const appGuideHeader = document.querySelector('.app-guide-header');
-
 const appMain = document.querySelector('.app-main');
+
+
 
 appTopbarNavicon.addEventListener("click", toggleMenu);
 

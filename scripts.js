fcc118cac1b5b7a15ae00422f0d2660ebbb00f17
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-11-29 01:18:00 -0500

    add variables

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
-console.log('foo');
+document.body.setAttribute('moo', ''); //set phony bbolean attribute
 
-document.body.setAttribute('moo', '');//set phony bbolean attribute
+const appSidebar = document.querySelector('.app-sidebar');
+const appScrim = document.querySelector('.app-scrim');
+const appTopbarNavicon = document.querySelector('.app-topbar-navicon');
+
+const appMiniGuide = document.querySelector('.app-mini-guide');
+
+const appSidebarContainer = document.querySelector('.app-sidebar-container');
+
+const appGuideNavicon = document.querySelector('.app-guide-navicon');
+const appGuideSpacer = document.querySelector('.app-guide-spacer');
+const appGuideHeader = document.querySelector('.app-guide-header');
+
+const appMain = document.querySelector('.app-main');
+
+appTopbarNavicon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    console.log('foo');
+    //   if(){
+    //
+    //   } else {
+    //
+    //   }
+}
